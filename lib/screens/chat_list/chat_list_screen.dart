78d4ac87857027615a1e289ca1ad52c736825914
Yader Campbell <yadermejiac123@gmail.com>
2023-09-05 10:39:10 -0600

    feat: List chats

--- lib/screens/chat_list/chat_list_screen.dart
+++ lib/screens/chat_list/chat_list_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_chat_bloc/blocs/chat/chat_bloc.dart';
 import 'package:flutter_chat_bloc/cubits/cubits.dart';
 import 'package:flutter_chat_bloc/screens/screens.dart';
+import 'package:flutter_chat_bloc/widgets/widgets.dart';
 
 import '../../blocs/blocs.dart';
+import '../../utils/utils.dart';
 
 class ChatListScreen extends StatelessWidget {
   const ChatListScreen({super.key});
@@ -12,27 +15,68 @@ class ChatListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //final authState = context.read<AuthBloc>().state;
+    final authBloc = context.read<AuthBloc>();
+    final currentUser = authBloc.state.user!;
+    final chatBloc = context.read<ChatBloc>();
+
     //eLog(authState);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("ChatList"),
-        actions: [
-          BlocConsumer<AuthBloc, AuthState>(
-            listener: (context, state) {
-              if(!state.isAuthenticated){
-                Navigator.of(context).pushReplacementNamed(GuestScreen.routeName);
-              }
-            },
-            builder: (context, state) {
-              return IconButton(
-                  onPressed: () {
-                    context.read<GuestCubit>().signOut();
-                  },
-                  icon: const Icon(Icons.logout));
-            },
-          )
-        ],
+    return StartUpContainer(
+      onInit: () async {
+        chatBloc.add(const ChatStarted());
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("ChatList"),
+              Text("User ID ${currentUser.email}",
+                  style: const TextStyle(
+                      fontSize: 14.0, fontWeight: FontWeight.w400)),
+            ],
+          ),
+          actions: [
+            BlocConsumer<AuthBloc, AuthState>(
+              listener: (context, state) {
+                if (!state.isAuthenticated) {
+                  Navigator.of(context)
+                      .pushReplacementNamed(GuestScreen.routeName);
+                }
+              },
+              builder: (context, state) {
+                return IconButton(
+                    onPressed: () {
+                      context.read<GuestCubit>().signOut();
+                    },
+                    icon: const Icon(Icons.logout));
+              },
+            )
+          ],
+        ),
+        body: BlocConsumer<ChatBloc, ChatState>(
+          listener: (_, __) {},
+          builder: (context, state) {
+            if(state.chats.isEmpty){
+              return const BlankContent(
+                content: "No chat available",
+                icon: Icons.chat_rounded,
+              );
+            }
+            return ListView.separated(
+              itemBuilder: (context, index) {
+                return Text("Hola");
+              },
+              separatorBuilder: (_, __) => const Divider(height: 1.5),
+              itemCount: state.chats.length
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            eLog("Show search user");
+          },
+          child: const Icon(Icons.search),
+        ),
       ),
     );
   }
